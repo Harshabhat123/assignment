a14c57438f062dd26aa583c8d260b2a6972e88e4
Joseph Fernandes <josferna@redhat.com>
2015-12-12 11:33:31 +0530

    rot13 : Implementaiton of rot13
    joe_rot13.c just has the functions that are required for
    encrypting and decrypting. The main() functon is missing as
    "Harsha" is expected to do the implememtion of that.
    
    The rot13 util from command line should be used as
    
    For Encyrption :
    rot13 -e <source_file_path> <destination_file_path>
    
    For Decryption :
    rot13 -d <source_file_path> <destination_file_path>
    
    if destination path is already existing then error out with
    warning. If the used wants to use then he/she should use
    -f force option
    
    eg:
    
    rot13 -f -e <source_file_path> <destination_file_path>
    
    of
    
    rot13 -f -d <source_file_path> <destination_file_path>
    
    Any other options or inputs should be rejected with the
    usage message.
    
    Signed-off-by: Joseph Fernandes <josferna@redhat.com>

---
+++ joe_rot13.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_VALUE 0xFF
+#define BLOCK_SIZE 1024
+
+typedef unsigned char uchar_t;
+typedef signed char schar_t;
+
+typedef enum boolean{
+        _false = 0,
+        _true
+} boolean_t;
+
+
+/**************************LOGGING*********************************************/
+/*logging related types, macros and functions*/
+typedef enum log_level {
+        log_error = -1,
+        log_info = 0
+}log_level_t;
+
+
+/* Function used for logging */
+void
+log_it (log_level_t     log_level,
+        const char       *file_name,
+        const char       *function,
+        int             line,
+        const char      *fmt, ...)
+{
+        FILE *stream = NULL;
+        va_list arg_list;
+        char *message = NULL;
+
+        if (log_level == -1)
+                stream = stderr;
+        else if (log_level == 0)
+                stream = stdout;
+
+        /* form the message */
+        va_start (arg_list, fmt);
+        vasprintf (&message, fmt, arg_list);
+        va_end (arg_list);
+
+        fprintf (stream, "%d %s %s : %s", line, file_name, function, message);
+
+        free (message);
+}
+
+
+/* Macro used for logging */
+#define LOG_IT(log_level, fmt...)\
+do {\
+        log_it (log_level, __FILE__, __FUNCTION__, __LINE__, ##fmt);\
+} while(0)
+
+/******************************************************************************/
+
+
+int
+rot13_encrypt (uchar_t *buffer,
+               ssize_t size)
+{
+
+        int         ret         = -1;
+        ssize_t i        = 0;
+
+        if (!buffer) {
+                LOG_IT (log_error, "Empty Buffer");
+                goto out;
+        }
+
+        if (size == 0) {
+                LOG_IT (log_error, "Buffer size is zero");
+                goto out;
+        } 
+
+        for (i = 0; i < size ; i++) {
+                *(buffer + i) = ((*buffer) + 13) % MAX_VALUE;
+        }
+
+        ret = 0;
+out:
+        return ret;
+
+}
+
+
+int
+ro13_decrypt (uchar_t *buffer,
+              ssize_t size)
+{
+
+        int              ret         = -1;
+        schar_t          diff        = 0;
+        int              i           = 0;
+
+        if (!buffer) {
+                LOG_IT (log_error, "Empty Buffer");
+                goto out;
+        }
+
+        if (size == 0) {
+                LOG_IT (log_error, "Buffer size is zero");
+                goto out;
+        }
+
+        for (i = 0; i < size ; i++) {
+                diff = (*buffer - 13);
+                if (diff < 0) {
+                        *buffer = MAX_VALUE + diff;
+                } else {
+                        *buffer = (uchar_t) diff;
+                }
+        }
+
+        ret = 0;
+out:
+        return ret;
+
+}
+
+/* function used for both encrypting and decrypting of file
+ * char *src_file_name  source file path
+ * char *des_file_name  destination file path
+ * boolean_t do_encrypt  if _true do encryption if not do decryption
+ * returns 0 on success and -ve value on failure */
+int
+crypt_rot13_file (const char    *src_file_name,
+                  const char    *des_file_name,
+                  boolean_t     do_encrypt)
+{
+
+        int ret                 = -1;
+        int src_fd                 = -1;
+        int des_fd                 = -1;
+        uchar_t buffer[1024]        = {0};
+        ssize_t buffer_len      = 0;
+
+        if (!src_file_name) {
+                LOG_IT (log_error, "Empty source file name");
+                goto out;
+        }
+
+        if (!des_file_name) {
+                LOG_IT (log_error, "Empty destination file name");
+                goto out;
+        }
+
+
+        src_fd = open (src_file_name, O_RDONLY);
+        if (src_fd < 0) {
+                LOG_IT (log_error, "Error opening source file %s : %s",
+                          src_file_name, strerror (errno));
+                goto out;
+        }
+
+        des_fd = open (des_file_name, O_WRONLY | O_CREAT | O_APPEND,
+                        S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        if (des_fd < 0) {
+                LOG_IT (log_error, "Error opening destination file %s : %s",
+                        des_file_name, strerror (errno));
+                goto out;
+        }
+
+        while ( (ret = read (src_fd, buffer, BLOCK_SIZE)) != 0 ) {
+                if (ret < 0) {
+                        LOG_IT (log_error,
+                                "Error reading from source file %s : %s",
+                                src_file_name, strerror (errno));
+                        goto out;
+                }
+
+                buffer_len = ret;
+
+                ret = (do_encrypt) ? rot13_encrypt (buffer, buffer_len) :
+                        ro13_decrypt (buffer, buffer_len);
+                if (ret) {
+                        LOG_IT (log_error, "Error %s the block",
+                                (do_encrypt) ? "encrypting" : "decrypting");
+                        goto out;
+                }
+
+                ret = write (des_fd, buffer, buffer_len);
+                if (ret < 0) {
+                        LOG_IT (log_error,
+                                "Error writing to the destination file %s : %s",
+                                des_file_name, strerror (errno));
+                        goto out;
+                }
+                
+        }
+
+        ret = 0;
+out:
+        /* Clean up on error */
+        if (ret) {
+                ret = remove(des_file_name);
+                if (ret) {
+                        LOG_IT (log_error,
+                                "Error deleting destination file %s : %s",
+                                des_file_name, strerror (errno));
+                        goto out;
+                }
+        }
+
+        if (src_fd >= 0) {
+                close (src_fd);
+        }
+
+        if (des_fd >= 0) {
+                close (des_fd);
+        }
+
+
+        return ret;
+}
+
+int
+main(int argc, char *argv [])
+{
+        int ret = -1;
+
+
+        ret = 0;
+out:
+        return ret;
+}
